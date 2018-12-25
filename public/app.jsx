efe3790de18f47cc2ab75015d69ade5e85bf2ea0
Christian Pena <christianp@tarte.com>
2018-12-24 22:20:55 -0500

    added state to the component so the name value can get updated depending on what is passed in the input field

--- public/app.jsx
+++ public/app.jsx
@@ -10,19 +10,31 @@ var Greeter = React.createClass({
             message: 'This is the default message'
         };
     },
+    getInitialState: function () {
+        return {
+            name: this.props.name
+        };
+    },
     onButtonClick: function (e) {
         e.preventDefault();
 
-        var name = this.refs.name.value;
-        alert(name);
+        var nameRef = this.refs.name;
+        var name = nameRef.value;
+        nameRef.value = '';
+
+        if (typeof name === 'string' && name.length > 0) {
+            this.setState({
+                name: name
+            });
+        }
     },
     render: function() {
-        var name = this.props.name;
+        var name = this.state.name;
         var message = this.props.message;
 
         return (
             <div>
-                <h1>Hello {name}</h1>
+                <h1>Hello {name}!</h1>
                 <p>{message}</p>
                 <form onSubmit={this.onButtonClick}>
                     <input type="text" ref="name"/>

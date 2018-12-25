569c186e1fe83a2cd3a33dd043f2f77e039bc9b0
Christian Pena <christianp@tarte.com>
2018-12-24 21:03:41 -0500

    created a simple component and made it render on the page

--- public/app.jsx
+++ public/app.jsx
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById('app'));
+/**
+ * React Component. Good practice to use uppercase on the react component
+ * You can only return one element, error will be thrown if multiple root
+ * html elements are returned
+ */
+var Greeter = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Hello React</h1>
+                <p>this is from the component</p>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Greeter/>, document.getElementById('app'));
